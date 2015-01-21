d273be369293a62a59aec4ebbc5312fa832c18ec
Amanda Pi <amandapi88@gmail.com>
2015-01-21 13:25:17 -0800

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Amanda Pi on 2015-01-21.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
